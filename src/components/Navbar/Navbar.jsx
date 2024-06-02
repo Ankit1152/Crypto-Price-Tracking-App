ad79146ee04a7b0413631d2dbab41f54260ec897
Ankit1152 <ankitkr1152@gmail.com>
2024-06-02 14:09:05 +0530

    Homepage completed and added footer component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import arrow_icon from "../../assets/arrow_icon.png";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd":
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+
+      case "eur":
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+
+      case "inr":
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+
+      default:
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+    }
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="" className="logo" />
@@ -16,7 +39,7 @@ const Navbar = () => {
       </ul>
 
       <div className="navbar-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EURO</option>
           <option value="inr">INR</option>
